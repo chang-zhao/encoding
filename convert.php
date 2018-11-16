b05d3bf5e2ff0aa08f8afbbe68d7d24e0390a150
Chang Zhao <chang-zhao@users.noreply.github.com>
2018-11-16 05:09:40 +0300

    This code actually works, unlike the previous mess

--- convert.php
+++ convert.php
@@ -1,40 +1,68 @@
 <?php
 /**
- * Convert file names written in UTF-8 in Windows by PHP 7.0-
- * (they look like "РєСѓР»РёРЅР°СЂРёСЏ.txt")
- * to file names in actual human-readable UTF-8, as PHP 7.1+ writes
+ * Bulk convert file names between encodings.
+ *
+ * For example, PHP versions prior to 7.1 wrote in Windows human-unreadable UTF-8
+ * (characters looking like "РєСѓР»РёРЅР°СЂРёСЏ.txt")
+ * But since 7.1 PHP writes UTF-8 in human-readable form, even in Windows.
+ *
+ * So we have a problem: UTF-8 encoded file names in Windows, written by PHP 7.0-,
+ * are unrecognizable for people AND FOR NEW PHP TOO.
+ * Hence we need to convert those gibberish file names into pretty ones.
  *
  * Run this file twice:
- * 1st in old PHP (before 7.1), to create a list of old file names with paths
- * 2nd in new PHP (7.1+), to create the same list of them but with the new encoding
+ * - 1st in old PHP (before 7.1), or in PHP with Windows encoding.
+ * => It would create a list of file names (full path)     // list1.txt
+ * => Also, a list of their sizes + timestamps, to check later against errors  // stat1.txt
+ * On completion, you could see the ending message - the amount of processed files.
  *
- * Then the lists will be compared, and files with non-ASCII names will be renamed
+ * - 2nd pass - in new PHP (7.1+), default (UTF-8) encoding.
+ * => It would create the same list of file names, but with the new encoding they
+ *    will be seen as gibberish    // list2.txt
+ * => Again, a list of their sizes + timestamps, to compare with stat1.txt     // stat2.txt
+ *
+ * Then the lists would be compared, and wjen you press "OK", the files with non-ASCII
+ * in their names will be renamed
+ *
+ * The same approach can be taken for reverse renaming or for other encodings.
+ *
  * Author: Chang Zhao
- * Date: 07.02.2018
+ * Date: 07.02.2018 - 16.11.2018
  */
-setlocale(LC_ALL, "C");                     // neutral
-ini_set('max_execution_time', 600);      // it can be long
-define('B', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-// default paths where to read/convert file names: *EDIT IF NECESSARY!*
+setlocale(LC_ALL, "C");                  // neutral locale
+ini_set('max_execution_time', 600);      // for many files it can take a long time
+define('B', dirname(__FILE__).DIRECTORY_SEPARATOR);	// base dir
+
+// Set here the dirs where we will process the files.
+// For DokuWiki, I set the following dirs:
+// -= * EDIT IF NECESSARY! * =-
+
 $dirPath = array('data'. DIRECTORY_SEPARATOR .'pages',
                  'data'. DIRECTORY_SEPARATOR .'media',
                  'data'. DIRECTORY_SEPARATOR .'meta',
                  'data'. DIRECTORY_SEPARATOR .'media-meta',
                  'data'. DIRECTORY_SEPARATOR .'media-attic',
                  'data'. DIRECTORY_SEPARATOR .'attic');
-$outFile1 = "list1.txt";    // file to list old file names
-$outStat1 = "stat1.txt";    // file to list old file sizes and timestamps (to check the correlations)
-$ready = 0;                 // flag: if old files are already listed, we could convert
-if (file_exists($outFile1)) {   // if old file names listed => list new ones
-    $outFile = "list2.txt";
-    $ready++;                   // flag +1
-    if (file_exists($outFile)) $ready += 4;
-} else $outFile = $outFile1;
+
+// For the first pass:
+
+$outName1 = "list1.txt";    // file to list old file names
+$outStat1 = "stat1.txt";    // file to list sizes and timestamps
+
+$ready = 0;                 // We increment this variable to track the progress
+
+if (file_exists($outName1)) {   // if the old file names have been listed
+    $outName = "list2.txt";     // then - the next step, we make the second list
+    $ready++;                   // progress mark +1
+    if (file_exists($outName)) $ready += 4;   // = both lists ready
+} else $outName = $outName1;                  // now $outName is 1st or 2nd,
+                                              // depending on which pass it is.
+
 if (file_exists($outStat1)) {   // if old file stats listed => list new ones
     $outStat = "stat2.txt";
-    $ready += 2;                // flag +2 => if flag == 3, both old lists are ready
-    if (file_exists($outStat)) $ready += 8;
+    $ready += 2;                // progress mark +2 => if it == 3, both old lists are ready
+    if (file_exists($outStat)) $ready += 8;    // 15 would mean both pairs of files are ready
 } else $outStat = $outStat1;
 
 ?>
@@ -45,6 +73,7 @@ if (file_exists($outStat1)) {   // if old file stats listed => list new ones
     <style type="text/css">
 .warn{ color: #A00000; }
 .deb { color: #6666CC; }
+.lo  { color: #AAA; }
     </style>
 </head>
 <body>
@@ -52,23 +81,25 @@ if (file_exists($outStat1)) {   // if old file stats listed => list new ones
 
 $s = '<p>Path: ';
 foreach($dirPath as $p) $s .= $p . ', ';
-echo rtrim($s, ', ') . '</p>' . PHP_EOL;
+echo rtrim($s, ', ') . '</p>' . PHP_EOL;   // For user convenience, print the paths we search
 
-if ($ready < 15) {      // flag: 0 = no files; 3 = 1st pair exists; 15 = both pairs exist
+if ($ready < 15) {      //  0 = no files; 3 = 1st pair exists; 15 = both pairs exist
 
-    // list the files - and save the list (names + stats) into a pair of files
+    // List the files. Save their names & stats into a pair of files
 
-//    if (!empty($_REQUEST['out1'])) $outFile = $_REQUEST['out1'];    // for tuning via URI
-//    if (!empty($_REQUEST['out2'])) $outStat = $_REQUEST['out2'];    // for tuning via URI
+    // if (!empty($_REQUEST['out1'])) $outName = $_REQUEST['out1'];    // could tune via URI
+    // if (!empty($_REQUEST['out2'])) $outStat = $_REQUEST['out2'];
     global $arr, $brr;              // arrays for files' names & files' stats
     foreach($dirPath as $dir) {     // fill those arrays
         dirToArray($dir);
     }
 
     echo '
-' . count($arr) .', '. count($brr) . '<hr>';
+File listing is done. ' . (($ready)? "2nd" : "1st" ) . ' pass OK.<br>
+Processed: ' . count($arr) .', '. count($brr) . '. Writing to files...
+<hr>';
 
-    $out1 = fopen($outFile, "w");   // and write them to files
+    $out1 = fopen($outName, "w");   // and write them to files
     $out2 = fopen($outStat, "w");
     $i = 0;
     foreach ($arr as $str) {
@@ -79,45 +110,83 @@ if ($ready < 15) {      // flag: 0 = no files; 3 = 1st pair exists; 15 = both pa
     fclose($out2);
 }
 
-// If no pairs of files existed already - having created the 1st pair, we done for now
+// Is now the first pass? => Then we done for now
 
-if ($ready < 3) die();
+if ($ready < 3) die("OK. Now run it in another encoding.</body></html>");
 
-$stat1 = file($outStat1, FILE_IGNORE_NEW_LINES);
 
-// Now if both pairs of files are ready, and "submit" was pressed, we can actually correlate & convert file names
+// This is the Second pass -> We go further.
+// Are we ready for renaming?
 
-if ($ready == 15 && $_REQUEST['convert'] == "go") {
-    $list1 = file($outFile1, FILE_IGNORE_NEW_LINES);
-    $list2 = file($outFile, FILE_IGNORE_NEW_LINES);
-//    array_multisort(array_map('strlen', $list1), SORT_DESC, $list1, $list2, $stat1);
-    for($i=0; $i<count($list1); $i++) {
-        if ($list1[$i] == $list2[$i]) continue;
-        echo '<p class="deb">' . $list2[$i] . ' &rarr; ' . $list1[$i] . '</p>' . PHP_EOL;
-        rename(str_replace('\\', '\\', B.$list2[$i]), str_replace('\\', '\\', B.$list1[$i]));
+// (1) If not yet -
 
-        if ($stat1[0] == DIRECTORY_SEPARATOR)		// is_dir => after dir rename its paths have changed
-            $list2 = str_replace($list2[$i] . DIRECTORY_SEPARATOR, $list1[$i] . DIRECTORY_SEPARATOR, $list2);
+if ($ready < 15 && (!$_REQUEST['convert'] || ($_REQUEST['convert'] !== "go"))) {
+
+    // Compare files' stats, to prevent mistakes
+    $stat1 = file($outStat1, FILE_IGNORE_NEW_LINES);
+    $stat2 = file($outStat, FILE_IGNORE_NEW_LINES);
+    $i = 0;
+    $diffs = array();
+    foreach($stat1 as $s) {
+        if ($s !== $stat2[$i]) $diffs[] = $i++;   // log: files of which ## are different
     }
-    die('<hr>'. $i);      // = total files processed
+
+    // if file stats are all equal, perhaps it's safe to rename
+    //  prepare the "Submit" button
+
+    if (!empty($diffs)) {
+        echo "<p class='warn'>Differences in file size/timestamp were detected.<br>
+File correlations can be wrong. See diff of list1.txt & list2.txt for difference.<br>
+The End...</p></body></html>";
+        die();
+    }
+?>
+<p>OK, old & new file lists seem to be identical (size & time).</p>
+<hr>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="hidden" name="convert" id="convert" value="go">
+    <input type="submit" value="Convert files' names">
+</form>
+</body>
+</html>
+<?php
 }
 
-// Otherwise prepare "submit"
+// Now if both pairs of files are ready, and "submit" was pressed, we could
+// actually compare stats & convert file names
 
-// Compare files' stats, to prevent mistaken renaming
+$list1 = file($outName1, FILE_IGNORE_NEW_LINES);
+$list2 = file($outName, FILE_IGNORE_NEW_LINES);
 
-$stat2 = file($outStat, FILE_IGNORE_NEW_LINES);
-$i = 0;
-$diffs = array();
-foreach($stat1 as $s) {
-    if ($s == $stat2[$i++]) continue;
-    $diffs[] = $i - 1;                  // in which lines files are different
+// In order to batch rename files in subfolders, we need to traverse dirs, starting from
+// the deepest levels, and renaming folders only after having all the files in them renamed.
+// So we go in the reverse order of the list, to ensure renaming parent dirs last.
+
+$n=count($list1);
+$i=$n;
+$j=0;
+$k=0;
+while (--$i >= 0) {
+    echo '<p class="deb">' . $list2[$i] . ' &rarr; ' . $list1[$i] . '</p>
+';
+    $chunks = explode(DIRECTORY_SEPARATOR, $list2[$i]);    // rename from
+    $from = array_pop($chunks);
+    $base = B.join(DIRECTORY_SEPARATOR, $chunks).DIRECTORY_SEPARATOR;
+    $to = array_pop(explode(DIRECTORY_SEPARATOR, $list1[$i])); // to
+    if ($from == $to) {
+        echo '<span class="lo">' . htmlspecialchars("$from = $to", ENT_QUOTES) . "Skip...</span>.<br>
+";
+        continue;
+    }
+    if (rename($base.$from, $base.$to)){
+        $j++;
+        echo htmlspecialchars($base.$to, ENT_QUOTES).'<br>
+';
+    }
+    else $k++;
 }
-
-// if file stats are all equal, perhaps it's safe to rename
-
-if (empty($diffs)) echo "<p>OK, files and new names seem to be corresponding (by filesize & timestamp).</p>";
-else die("<p class='warn'>Differences in file size/timestamp were detected. Files' correlations can be wrong.</p>");
+echo "<hr>
+Total files: $n; renamed: $j; errors: $k.</body></html>";
 
 /**
  * Lists all files and dirs under the path
@@ -141,10 +210,3 @@ function dirToArray($dir) {
 }
 
 ?>
-<hr>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <input type="hidden" name="convert" id="convert" value="go">
-    <input type="submit" value="Convert files' names">
-</form>
-</body>
-</html>
